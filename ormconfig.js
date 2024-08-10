d5fd768b48adc8f4348796f18e6860e978dc0a62
DOOMSDAY101 <ifeoluwasulaiman30@gmail.com>
2024-08-10 21:05:13 +0100

    fix: added some fixes

--- ormconfig.js
+++ ormconfig.js
@@ -2,7 +2,7 @@ const { DataSource } = require("typeorm")
 const AppDataSource = new DataSource({
   type: 'mysql',
   host: process.env.DB_HOST,
-  // port: process.env.DB_PORT,
+  port: process.env.DB_PORT,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
